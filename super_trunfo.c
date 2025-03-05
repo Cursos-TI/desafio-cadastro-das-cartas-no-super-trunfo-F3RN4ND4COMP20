0a5f7a95beb722899649b2480a63e9138288df3b
F3RN4ND4COMP20 <mariafernanda.cravodias@gmail.com>
2025-03-05 03:19:02 +0000

    troquei o especificador de formato %.2f pelo %f no printf

--- super_trunfo.c
+++ super_trunfo.c
@@ -4,12 +4,20 @@ int main(){
     char Estado1, Estado2[10];
     char Codigocarta1[10], Codigocarta2[10];
     char Nomecidade1[20], Nomecidade2[20];
-    int Populacao1, Populacao2;
+    unsigned long int Populacao1, Populacao2;
     float Areacidade1, Areacidade2;
     float PIB1, PIB2;
     int Pontosturisticos1, Pontosturisticos2;
     float Densidadepopulacional1, Densidadepopulacional2;
     float PIBpercapita1, PIBpercapita2;
+    float Superpoder1, Superpoder2;
+    int Resultado1;
+    int Resultado2;
+    int Resultado3;
+    int Resultado4;
+    int Resultado5;
+    int Resultado6;
+    int Resultado7;
 
     printf("Carta 1\n");
     printf("Digite o Estado: ");
@@ -22,7 +30,7 @@ int main(){
     scanf("%s", Nomecidade1);
 
     printf("Digite a Populacao: ");
-    scanf("%d", &Populacao1);
+    scanf("%lu", &Populacao1);
 
     printf("Digite a Area da cidade: ");
     scanf("%f", &Areacidade1);
@@ -39,19 +47,24 @@ int main(){
     printf("Digite o PIB per capita: ");
     scanf("%f", &PIBpercapita1);
 
+    printf("Digite o Super poder: ");
+    scanf("%f", &Superpoder1);
+
     Densidadepopulacional1=(float)(Populacao1/Areacidade1);
     PIBpercapita1=(float)(PIB1/Populacao1);
+    Superpoder1=(float)(Populacao1+Areacidade1+PIB1+Pontosturisticos1+PIBpercapita1+1/Densidadepopulacional1);
 
     printf("Carta 1\n");
     printf("Estado: %c\n", Estado1);
     printf("Codigo da carta: %s\n", Codigocarta1);
     printf("Nome da cidade: %s\n", Nomecidade1);
-    printf("Populacao: %d\n", Populacao1);
-    printf("Area da cidade: %f\n", Areacidade1);
-    printf("PIB: %f\n", PIB1);
+    printf("Populacao: %lu\n", Populacao1);
+    printf("Area da cidade: %.3f\n", Areacidade1);
+    printf("PIB: %.2f\n", PIB1);
     printf("Pontos turisticos: %d\n", Pontosturisticos1);
-    printf("Densidade populacional: %.2f\n", Densidadepopulacional1);
-    printf("PIB per capita: %.2f\n", PIBpercapita1);
+    printf("Densidade populacional: %f\n", Densidadepopulacional1);
+    printf("PIB per capita: %f\n", PIBpercapita1);
+    printf("Super poder: %f\n", Superpoder1);
 
     printf("Carta 2\n");
     printf("Digite o Estado: ");
@@ -64,7 +77,7 @@ int main(){
     scanf("%s", Nomecidade2);
 
     printf("Digite a Populacao: ");
-    scanf("%d", &Populacao2);
+    scanf("%lu", &Populacao2);
 
     printf("Digite a Area da cidade: ");
     scanf("%f", &Areacidade2);
@@ -81,19 +94,49 @@ int main(){
     printf("Digite o PIB per capita: ");
     scanf("%f", &PIBpercapita2);
 
+    printf("Digite o Super poder: ");
+    scanf("%f", &Superpoder2);
+
     Densidadepopulacional2=(float)(Populacao2/Areacidade2);
     PIBpercapita2=(float)(PIB2/Populacao2);
+    Superpoder2=(float)(Populacao2+Areacidade2+PIB2+Pontosturisticos2+PIBpercapita2+1/Densidadepopulacional2);
 
     printf("Carta 2\n");
     printf("Estado: %s\n", Estado2);
     printf("Codigo da carta: %s\n", Codigocarta2);
     printf("Nome da cidade: %s\n", Nomecidade2);
-    printf("Populacao: %d\n", Populacao2);
-    printf("Area da cidade: %f\n", Areacidade2);
-    printf("PIB: %f\n", PIB2);
+    printf("Populacao: %lu\n", Populacao2);
+    printf("Area da cidade: %.3f\n", Areacidade2);
+    printf("PIB: %.2f\n", PIB2);
     printf("Pontos turisticos: %d\n", Pontosturisticos2);
-    printf("Densidade populacional: %.2f\n", Densidadepopulacional2);
-    printf("PIB per capita: %.2f\n", PIBpercapita2);
+    printf("Densidade populacional: %f\n", Densidadepopulacional2);
+    printf("PIB per capita: %f\n", PIBpercapita2);
+    printf("Super poder: %f\n", Superpoder2);
+
+    // Comparacao das cartas
+
+    Resultado1=Populacao1>Populacao2;
+    Resultado2=Areacidade1>Areacidade2;
+    Resultado3=PIB1>PIB2;
+    Resultado4=Pontosturisticos1>Pontosturisticos2;
+    Resultado5=PIBpercapita1>PIBpercapita2;
+    Resultado6=Densidadepopulacional1<Densidadepopulacional2;
+    Resultado7=Superpoder1>Superpoder2;
+
+    printf("A Carta 1 vence");
+    printf("A Carta 2 vence");
+    printf("A Carta 1 vence");
+    printf("A Carta 2 vence");
+    printf("A Carta 1 vence");
+    printf("A Carta 2 vence");
+    printf("A Carta 1 vence");
+    printf("A Carta 2 vence");
+    printf("A Carta 1 vence");
+    printf("A Carta 2 vence");
+    printf("A Carta 1 vence");
+    printf("A Carta 2 vence");
+    printf("A Carta 1 vence");
+    printf("A Carta 2 vence");
 
     return 0;
 
